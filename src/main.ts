c43b3459a2cd61ea4a751fad3e63f600d8e2c482
SrMashida <flavio.pprado4@gmail.com>
2021-11-01 00:04:04 -0300

    feature/GL9: starting the gRPC.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
 import * as csurf from 'csurf';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'user',
+        protoPath: join(__dirname, 'proto/user/user.proto'),
+      },
+    },
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Generic Login')
