dbae6652f1d4a3654acd165e946eb634cd89f67e
SrMashida <flavio.pprado4@gmail.com>
2021-11-03 15:37:39 -0300

    feature/GL11: starting gRPC.

---
+++ dist/modules/user/grpc/grpc-user.options.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.grpcClientOptions = void 0;
+const microservices_1 = require("@nestjs/microservices");
+const path_1 = require("path");
+exports.grpcClientOptions = {
+    transport: microservices_1.Transport.GRPC,
+    options: {
+        url: 'localhost:3000',
+        package: 'user',
+        protoPath: (0, path_1.join)(__dirname, './user.proto'),
+    },
+};
+//# sourceMappingURL=grpc-user.options.js.map
